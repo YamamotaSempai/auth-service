2f1197724208f0f3ad75c8a4478e5447d787ff7c
Zhantleu <myLittlePoni>
2021-09-27 23:12:03 +0600

    fix(pom): rename service name for docker file

--- src/main/kotlin/kz/aa/auth_api/repositories/CityRepository.kt
+++
@@ -1,7 +0,0 @@
-package kz.aa.auth_api.repositories
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface CityRepository : JpaRepository<City, Long>
