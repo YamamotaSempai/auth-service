758160e50b3457baa9b2940022d05ed816359511
Zhantleu <myLittlePoni>
2021-09-28 09:50:28 +0600

    feat(dto): add valid annotations

--- src/main/kotlin/kz/aa/auth_api/models/dto/LoginDto.kt
+++ src/main/kotlin/kz/aa/auth_api/models/dto/LoginDto.kt
@@ -1,8 +1,11 @@
 package kz.aa.auth_api.models.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import java.io.Serializable
 
 class LoginDto(
+    @JsonProperty(required = true)
     val login: String,
+    @JsonProperty(required = true)
     val password: String
 ) : Serializable
